8a930cb7baa4ffe506910f32cad5780ab8c18e71
김현석 <kimhyeonseok@gimhyeonseog-ui-MacBook-Pro.local>
2017-08-11 10:10:53 +0900

    Initial Commit

---
+++ moneyConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  moneyConverter
+//
+//  Created by 김현석 on 2017. 8. 11..
+//  Copyright © 2017년 Kimhyeonseok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
